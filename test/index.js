f50a60640c44a9f8539a6ca628d1ac05c9a9c9eb
Daniel Cousens <github@dcousens.com>
2017-04-02 13:13:53 +1000

    tests: checksum tests first

--- test/index.js
+++ test/index.js
@@ -2,12 +2,21 @@ let tape = require('tape')
 let fixtures = require('./fixtures')
 let bech32 = require('../')
 
+fixtures.validChecksum.forEach((f) => {
+  let string = f.toLowerCase()
+
+  tape(string + ' has a valid checksum', (t) => {
+    t.plan(1)
+    t.ok(bech32.decode(string))
+  })
+})
+
 fixtures.valid.forEach((f) => {
   // unlike the reference impl., we don't support mixed/uppercase
   let string = f.string.toLowerCase()
   let buffer = Buffer.from(f.hex, 'hex')
 
-  tape('encode', (t) => {
+  tape('encode ' + string, (t) => {
     t.plan(1)
     t.same(bech32.encode(f.prefix, buffer), string)
   })
@@ -20,15 +29,5 @@ fixtures.valid.forEach((f) => {
       prefix: f.prefix,
       data: Buffer.from(f.hex, 'hex')
     }, decode)
-  })
-})
-
-fixtures.validChecksum.forEach((f) => {
-  tape('decode', (t) => {
-    t.plan(1)
-
-    // unlike the reference impl., we don't support mixed/uppercase
-    let string = f.toLowerCase()
-    t.ok(bech32.decode(string))
   })
 })
