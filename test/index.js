53965841cca0c5bf229b9b2d5999b791e2ed8747
Thomas Kerin <thomas.kerin@bitmaintech.com>
2017-08-06 14:46:14 +0200

    Expose convertBits, remove segwit address functions & add tests

--- test/index.js
+++ test/index.js
@@ -1,25 +1,54 @@
 'use strict'
 let tape = require('tape')
+let assert = require('assert')
 let fixtures = require('./fixtures')
 let bech32 = require('../')
 
+fixtures.bech32.valid.forEach((f, i) => {
+  let good = f.string.toLowerCase()
+  let pos = good.lastIndexOf('1')
+  let bad = good.slice(0, pos + 1) +
+    String.fromCharCode(good.charCodeAt(pos + 1) ^ 1) +
+    good.slice(pos + 2)
+
+  tape('succeeds with a valid checksum: ' + good, (t) => {
+    t.plan(1)
+    t.doesNotThrow(function () {
+      bech32.decode(good)
+    })
+  })
+
+  tape('fails with incorrect checksum: ' + bad, (t) => {
+    t.plan(1)
+    t.throws(function () {
+      bech32.decode(bad)
+    })
+  })
+})
+
 fixtures.addresses.valid.forEach((f, i) => {
   // unlike the reference impl., we don't support mixed/uppercase
-  let string = f.string.toLowerCase()
+  let string = f.string.toLowerCase();
   let buffer = Buffer.from(f.hex, 'hex')
 
   tape('encode ' + string, (t) => {
     t.plan(1)
-    t.same(bech32.encode(f.prefix, f.version, buffer), string)
+    let data = [f.version].concat(bech32.convertBits(buffer, 8, 5, true))
+    t.same(bech32.encode(f.prefix, data), string)
   })
 
   tape('decode ' + string, (t) => {
-    t.plan(1)
+    t.plan(2)
 
-    let actual = bech32.decode(f.prefix, string)
+    let actual = bech32.decode(string)
+    let data = [f.version].concat(bech32.convertBits(buffer, 8, 5, true))
+
     t.same(actual, {
-      version: f.version,
-      program: buffer
+      prefix: f.prefix,
+      bitData: data
     })
+
+    let addr = bech32.encode(f.prefix, data)
+    t.same(addr, string)
   })
 })
