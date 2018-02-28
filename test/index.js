6d0ea828e956047abb304e661c87a26bbf202917
Kirill Fomichev <fanatid@ya.ru>
2018-02-28 12:13:58 +0300

    Add unsafe functions

--- test/index.js
+++ test/index.js
@@ -19,19 +19,23 @@ fixtures.bech32.valid.forEach((f) => {
   })
 
   tape(`decode ${f.string}`, (t) => {
-    t.plan(1)
-    t.same(bech32.decode(f.string, f.limit), {
+    t.plan(2)
+
+    var expected = {
       prefix: f.prefix.toLowerCase(),
       words: f.words
-    })
+    }
+    t.same(bech32.decodeUnsafe(f.string, f.limit), expected)
+    t.same(bech32.decode(f.string, f.limit), expected)
   })
 
   tape(`fails for ${f.string} with 1 bit flipped`, (t) => {
-    t.plan(1)
+    t.plan(2)
 
     let buffer = Buffer.from(f.string, 'utf8')
     buffer[f.string.lastIndexOf('1') + 1] ^= 0x1 // flip a bit, after the prefix
     let string = buffer.toString('utf8')
+    t.equal(bech32.decodeUnsafe(string, f.limit), undefined)
     t.throws(function () {
       bech32.decode(string, f.limit)
     }, new RegExp('Invalid checksum|Unknown character'))
@@ -53,7 +57,8 @@ fixtures.bech32.invalid.forEach((f) => {
     let string = f.string || Buffer.from(f.stringHex, 'hex').toString('binary')
 
     tape(`decode fails for ${string} (${f.exception})`, (t) => {
-      t.plan(1)
+      t.plan(2)
+      t.equal(bech32.decodeUnsafe(string), undefined)
       t.throws(function () {
         bech32.decode(string)
       }, new RegExp(f.exception))
@@ -63,7 +68,8 @@ fixtures.bech32.invalid.forEach((f) => {
 
 fixtures.fromWords.invalid.forEach((f) => {
   tape(`fromWords fails with ${f.exception}`, (t) => {
-    t.plan(1)
+    t.plan(2)
+    t.equal(bech32.fromWordsUnsafe(f.words), undefined)
     t.throws(function () {
       bech32.fromWords(f.words)
     }, new RegExp(f.exception))
