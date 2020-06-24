5ffd656ced69a9f1a489d7f2b34c30ea34568d70
Paul Miller <paul@paulmillr.com>
2020-06-24 17:30:28 +0300

    Target es2015.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const BECH32_SEPARATOR = '1';
 const BECH32_MAX_LIMIT = 90;
 const BECH32_MIN_LIMIT = 8;
 const ALPHABET = 'qpzry9x8gf2tvdw0s3jn54khce6mua7l';
-const ALPHABET_MAP = [...ALPHABET].reduce((res, el, i) => ({ ...res, [el]: i }), {});
+const ALPHABET_MAP = [...ALPHABET].reduce((res, el, i) => (Object.assign(Object.assign({}, res), { [el]: i })), {});
 const GENERATORS = [0x3b6a57b2, 0x26508e6d, 0x1ea119fa, 0x3d4233dd, 0x2a1462b3];
 function polymodStep(pre) {
     const b = pre >> 25;
