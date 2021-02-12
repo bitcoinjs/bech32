5b83a30e44894df8b2876b3b34b8053aa6a28ab0
Andrew Toth <andrewstoth@gmail.com>
2021-02-12 12:35:44 -0500

    Add typescript tests for BIP350 bech32m

--- test/types.ts
+++ test/types.ts
@@ -4,39 +4,39 @@
 
 import * as bech32 from "..";
 
-function encodeUint8Array(prefix: string, data: Uint8Array): string {
-  const address = bech32.encode(prefix, bech32.toWords(data));
+function encodeUint8Array(prefix: string, data: Uint8Array, encoding: bech32.Encoding): string {
+  const address = bech32.encode(prefix, bech32.toWords(data), encoding);
   return address;
 }
 
-function decodeUint8Array(address: string): { readonly prefix: string; readonly data: Uint8Array } {
-  const decodedAddress = bech32.decode(address);
+function decodeUint8Array(address: string, encoding: bech32.Encoding): { readonly prefix: string; readonly data: Uint8Array } {
+  const decodedAddress = bech32.decode(address, encoding);
   return {
     prefix: decodedAddress.prefix,
     data: new Uint8Array(bech32.fromWords(decodedAddress.words)),
   };
 }
 
-function encodeBuffer(prefix: string, data: Buffer): string {
-  const address = bech32.encode(prefix, bech32.toWords(data));
+function encodeBuffer(prefix: string, data: Buffer, encoding: bech32.Encoding): string {
+  const address = bech32.encode(prefix, bech32.toWords(data), encoding);
   return address;
 }
 
-function decodeBuffer(address: string): { readonly prefix: string; readonly data: Buffer } {
-  const decodedAddress = bech32.decode(address);
+function decodeBuffer(address: string, encoding: bech32.Encoding): { readonly prefix: string; readonly data: Buffer } {
+  const decodedAddress = bech32.decode(address, encoding);
   return {
     prefix: decodedAddress.prefix,
     data: Buffer.from(bech32.fromWords(decodedAddress.words)),
   };
 }
 
-function encodeUnsafe(prefix: string, data: Uint8Array): string | undefined {
-  const address = bech32.encode(prefix, bech32.toWordsUnsafe(data));
+function encodeUnsafe(prefix: string, data: Uint8Array, encoding: bech32.Encoding): string | undefined {
+  const address = bech32.encode(prefix, bech32.toWordsUnsafe(data), encoding);
   return address;
 }
 
-function decodeUnsafe(address: string): { readonly prefix: string; readonly data: Uint8Array } {
-  const decodedAddress = bech32.decodeUnsafe(address);
+function decodeUnsafe(address: string, encoding: bech32.Encoding): { readonly prefix: string; readonly data: Uint8Array } {
+  const decodedAddress = bech32.decodeUnsafe(address, encoding);
   return {
     prefix: decodedAddress.prefix,
     data: new Uint8Array(bech32.fromWordsUnsafe(decodedAddress.words)),
@@ -47,22 +47,25 @@ function main(): void {
   {
     const prefix = "foo";
     const data = new Uint8Array([0x00, 0x11, 0x22]);
-    const address = encodeUint8Array(prefix, data);
-    const decoded = decodeUint8Array(address);
+    const encoding = bech32.Encoding.BECH32;
+    const address = encodeUint8Array(prefix, data, encoding);
+    const decoded = decodeUint8Array(address, encoding);
     console.log(prefix, data, address, decoded);
   }
   {
     const prefix = "foo";
     const data = Buffer.from([0x00, 0x11, 0x22]);
-    const address = encodeBuffer(prefix, data);
-    const decoded = decodeBuffer(address);
+    const encoding = bech32.Encoding.BECH32;
+    const address = encodeBuffer(prefix, data, encoding);
+    const decoded = decodeBuffer(address, encoding);
     console.log(prefix, data, address, decoded);
   }
   {
     const prefix = "foo";
     const data = new Uint8Array([0x00, 0x11, 0x22]);
-    const address = encodeUnsafe(prefix, data);
-    const decoded = decodeUnsafe(address);
+    const encoding = bech32.Encoding.BECH32;
+    const address = encodeUnsafe(prefix, data, encoding);
+    const decoded = decodeUnsafe(address, encoding);
     console.log(prefix, data, address, decoded);
   }
 }
